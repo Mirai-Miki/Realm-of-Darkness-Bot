362d9b5d748cce52f8fb6d774b22358a2b3fb8e7
Michael Bossner <Michael.bossner@hotmail.com>
2021-10-25 21:19:12 +1000

    v3 042
    updated activity

--- modules/util/setActivity.js
+++ modules/util/setActivity.js
@@ -3,7 +3,6 @@ module.exports = function (client)
 {
 	let userCount = 0;
     client.guilds.cache.each(guild => userCount += guild.memberCount);
-    client.user.setActivity(`${client.guilds.cache.size} Chronicles and` +
-        ` ${userCount} Players | Type /rod for a list of commands`, 
+    client.user.setActivity(`${client.guilds.cache.size} Chronicles`, 
         { type: 'WATCHING' });
 }
