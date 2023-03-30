a09a6030b527519e6fe5b52b37b9136e3c8ef918
Mirai-Miki <michael.bossner@hotmail.com>
2023-03-30 16:13:20 +1000

    v4.0.1
    fixed a bug in is Admin or ST
    fixed shard bugs if not used from a shard

--- modules/isAdminOrST.js
+++ modules/isAdminOrST.js
@@ -1,12 +1,13 @@
 'use strict'
 const API = require('../realmAPI');
+const { PermissionFlagsBits } = require('discord.js');
 
 module.exports = async function isAdminOrST(member, guildId)
 {
   if (!member || !guildId) throw new Error('Need a member && guildID');
 
   const roles = await API.getSTRoles(guildId);
-  if (!member.permissions.has("ADMINISTRATOR") && 
+  if (!member.permissions.has(PermissionFlagsBits.Administrator) && 
     !member.roles.cache.hasAny(...roles))
   {
     return false;
--- modules/setActivity.js
+++ modules/setActivity.js
@@ -3,14 +3,19 @@ const { ActivityType } = require('discord.js')
 
 module.exports = async function setActivity(client) 
 {
-  const guildSizes = await client.shard.fetchClientValues('guilds.cache.size');
-  const size = guildSizes.reduce(
-    ((accumulator, currentValue) => accumulator + currentValue), 0
-  );
+  let guildSizes;
+  let size;
+  if (client.shard)
+  {  
+    guildSizes = await client.shard.fetchClientValues('guilds.cache.size');
+    size = guildSizes.reduce(
+      ((accumulator, currentValue) => accumulator + currentValue), 0);
+  }
+  else size = client.guilds.cache.size;  
   
   client.user.setActivity({ 
-    name: `${size} Chronicles`, 
+    name: `${size} Chronicles - Shard: ${client.shard?.ids ?? 'None'}`, 
     type: ActivityType.Watching, 
-    shard: client.shard.ids 
+    shard: client.shard?.ids ?? null
   });
 }
