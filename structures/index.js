fd7d013a3d150174c376787ec42acfedd64593fb
Mirai-Miki <michael.bossner@hotmail.com>
2023-03-30 13:56:52 +1000

    v4.0.1
    -fixed a module error

--- structures/index.js
+++ structures/index.js
@@ -2,7 +2,7 @@
 
 module.exports.DamageTracker5th = require('./DamageTracker5th');
 module.exports.DamageTracker20th = require('./DamageTracker20th');
-module.exports.Humanity5th = require('./Humanity5th');
+module.exports.Humanity5th = require('./humanity5th');
 module.exports.Counter = require('./Counter');
 module.exports.Consumable = require('./Consumable');
 module.exports.Quintessence = require('./Quintessence')
