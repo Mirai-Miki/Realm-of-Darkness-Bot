f8393b0e4727778ce8dd60c64014a949c8c565db
Mirai-Miki <michael.bossner@hotmail.com>
2023-03-30 14:08:00 +1000

    v4.0.0 2
    require changed

--- modules/dice/index.js
+++ modules/dice/index.js
@@ -2,9 +2,9 @@
 
 exports.vtmRoll = require('./5th/vtmRoll');
 exports.hunterDice = require('./5th/HunterRoll');
-exports.rouse = require('./5th/rouse');
-exports.resonance = require('./5th/resonance');
-exports.generalRoll = require('./generalRoll');
+exports.rouse = require('./5th/Rouse');
+exports.resonance = require('./5th/Resonance');
+exports.generalRoll = require('./GeneralRoll');
 exports.roll20th = require('./roll20th');
 exports.roll20thInit = require('./roll20thInit');
 exports.Roll = require('./Roll');
