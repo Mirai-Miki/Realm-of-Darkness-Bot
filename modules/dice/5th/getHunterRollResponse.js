075ce7a00edb9332689a70e1c23272e6250038f6
Mirai-Miki <michael.bossner@hotmail.com>
2023-03-30 14:57:05 +1000

    v4.0.0 7
    trying to fix another require issue

--- modules/dice/5th/getHunterRollResponse.js
+++ modules/dice/5th/getHunterRollResponse.js
@@ -2,7 +2,7 @@
 const { EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder,
   ButtonBuilder, ButtonStyle } = require('discord.js');
 const { Emoji } = require('../../../Constants');
-const HunterV5RollResults = require('../../../structures/HunterV5RollResults');
+const HunterV5RollResults = require('../../../structures/hunterV5RollResults');
 
 module.exports.getEmbed = function(interaction)
 {
