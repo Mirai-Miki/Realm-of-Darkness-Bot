63f7f07bec32a3c499cece61070b78cb26aa4cc6
Mirai-Miki <michael.bossner@hotmail.com>
2023-11-30 19:33:03 +1000

    v4.4.0 bug fix (2)
    - added a wait for setting activity to avoid spawning errors

--- events/ready.js
+++ events/ready.js
@@ -4,14 +4,19 @@ const setActivity = require('../modules/setActivity');
 const updateAllGuilds = require('../modules/updateAllGuilds');
 const API = require('../realmAPI');
 
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	async execute(client) {
-		await setActivity(client);
-		setInterval(() => { setActivity(client) }, 300000);
 		await API.updateBot(client);
 		await updateAllGuilds(client);
 		console.log(`Connected as: ${client.user.tag} || Shard: ${client.shard?.ids}`);
+		sleep(30000)
+		await setActivity(client);
+		setInterval(() => { setActivity(client) }, 300000);
 	},
 };
